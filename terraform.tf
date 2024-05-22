198fd35a903ecf8d2748fea2c7fa62bad522f236
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-05-21 22:19:29 -0700

    test dynamic tf backend

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers {
+        aws = {
+            version = "~> 4.59.0"
+        }
+    }
+    backend "s3" {
+        encrypt        = true
+    }
+}
+
+provider "aws" {
+    # Set to us-east-1 as this is where resources have to live
+    # for the ACM certificate to be attached to CloudFront
+    region = "us-east-1"
+}
+
