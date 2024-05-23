655f4a2e0228114d8081e70100fc1fc482079788
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-05-22 22:08:55 -0700

    test action again

--- terraform.tf
+++ terraform.tf
@@ -9,9 +9,3 @@ terraform {
     }
 }
 
-provider "aws" {
-    # Set to us-east-1 as this is where resources have to live
-    # for the ACM certificate to be attached to CloudFront
-    region = "us-east-1"
-}
-
