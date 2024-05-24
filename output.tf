904d4366c4571ba73ac3761f7a9a2cbf24f6dfd1
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-05-23 22:52:04 -0700

    add tf dynamodb output and add environment variables

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "bucket" {
   description = "The S3 bucket to store the remote state file."
   value       = aws_s3_bucket.state.id
 }
+
+output "dynamodb" {
+  description = "The Dynamodb Table for the lock"
+  value       = aws_dynamodb_table.lock.name
+} 
